3da6e986ed1a5a116e91f38f3180342d7a45ae5c
Alejandro Báez <baezdalex@gmail.com>
2015-07-12 18:05:41 -0500

    :)

--- js/init.js
+++ js/init.js
@@ -1,36 +1,3 @@
-/*(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
- $(document).ready(function(){
-    //$(".button-collapse").sideNav();
-    //$(".dropdown-button").dropdown();
-    $('ul.tabs').tabs('select_tab', 'tab_id');
-    $('.slider').slider({full_width: true});
-
-  $(".button-collapse").sideNav();
-
-  $('.dropdown-button').dropdown({'hover': false});
-
-  $('select').not('.disabled').material_select();
- 
-  });
-// Initialize collapse button
-  
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  //$('.collapsible').collapsible();
-//efecto scroll
-var options = [ 
-    {selector: '#image-test', offset: 500, callback: 'Materialize.fadeInImage("#image-test")' },
-{selector: '#image-test2', offset: 500, callback: 'Materialize.fadeInImage("#image-test2")'},
-{selector: '#image-test3', offset: 500, callback: 'Materialize.fadeInImage("#image-test3")' }]; 
-Materialize.scrollFire(options); */
-
 $(document).ready(function()
 { 
     $('.button-collapse').sideNav();
@@ -38,7 +5,9 @@ $(document).ready(function()
     $('select').not('.disabled').material_select();
     $('.parallax').parallax();
      $('ul.tabs').tabs('select_tab', 'tab_id');
-    $('.slider').slider({full_width: true});
+    $('.slider').slider({height: 450});
+    $('.collapsible').collapsible({accordion : false });
+    $('.materialboxed').materialbox();
 
   
 });
