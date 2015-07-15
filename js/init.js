f149871292d7807ee79190c48605e05d4939a783
Alejandro Báez <baezdalex@gmail.com>
2015-07-15 16:00:02 -0500

    modificando contenido de jasmo.html

--- js/init.js
+++ js/init.js
@@ -4,17 +4,19 @@ $(document).ready(function()
     $('.dropdown-button').dropdown({'hover': false});
     $('select').not('.disabled').material_select();
     $('.parallax').parallax();
-     $('ul.tabs').tabs('select_tab', 'tab_id');
+    $('ul.tabs').tabs('select_tab', 'tab_id');
     $('.slider').slider({height: 450});
     $('.collapsible').collapsible({accordion : false });
     $('.materialboxed').materialbox();
-     $('.modal-trigger').leanModal();
+    $('.modal-trigger').leanModal();
   
 });
 
 //efecto scroll
+/*
 var options = [ 
     {selector: '#image-test', offset: 500, callback: 'Materialize.fadeInImage("#image-test")' },
 {selector: '#image-test2', offset: 500, callback: 'Materialize.fadeInImage("#image-test2")'},
 {selector: '#image-test3', offset: 500, callback: 'Materialize.fadeInImage("#image-test3")' }]; 
 Materialize.scrollFire(options);
+*/
