c6f6df4cbf4de1e7b8c607f8e219d9654b271152
CarlosVazGom <carlosvazquezgom@gmail.com>
2015-07-24 02:14:14 -0500

    implementación de la validación Float
    Validación de las asignaciones float pero aun no se como recuperar el
    valor completo de la variable solo me recupera los últimos 5 dígitos y
    la variable char no permite valores numéricos al igual que int y float
    no permiten un valor tipo char

--- Num.java
+++ Num.java
@@ -7,5 +7,14 @@ public class Num {
         }catch(NumberFormatException nfe){
             return false;
         }
-    }    
+    }
+    
+    public static boolean valFloat(String var){
+        try{
+            Float.parseFloat(var);
+            return true;
+        }catch(NumberFormatException nfe){
+            return false;
+        }
+    }
 }
